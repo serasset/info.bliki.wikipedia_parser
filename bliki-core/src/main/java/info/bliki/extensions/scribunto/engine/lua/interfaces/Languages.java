f4f4449e405d14c530e16ff781c232f7d6db11ee
Gilles Sérasset <gilles.serasset@imag.fr>
2023-07-05 20:28:37 +0200

    fix(mw.language): Implementing fetchLanguageName

--- bliki-core/src/main/java/info/bliki/extensions/scribunto/engine/lua/interfaces/Languages.java
+++ bliki-core/src/main/java/info/bliki/extensions/scribunto/engine/lua/interfaces/Languages.java
@@ -2,6 +2,7 @@ package info.bliki.extensions.scribunto.engine.lua.interfaces;
 
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 final class Languages {
@@ -17,6 +18,15 @@ final class Languages {
      * @return string: Language name or empty
      */
     public String getName(String code, String inLanguage) {
-        return codes.get(code);
+        Locale l = new Locale(code);
+        if (inLanguage == null) {
+            // Autonym case
+            return l.getDisplayLanguage(l);
+        } else {
+            return l.getDisplayLanguage(new Locale(inLanguage));
+        }
+        // return codes.get(code);
     }
+
+
 }
